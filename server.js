99336a68b851e1ada0a2775c4de9a358c741bfe4
Gangadhararao Rayudu <Gangadhararao.Rayudu@katalystpartners.com>
2026-04-14 05:10:25 -0400

    testcase

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello this is the first successful domain");
+  res.end("What are the possibilites");
 });
 
 server.listen(3000, () => {
